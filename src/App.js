016f84b9e8e6e5a6d57c7b322dcf1e05431f32f3
mohiiit <mohit18343@iiitd.ac.in>
2024-04-20 12:17:40 +0530

    basic change in the app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hello from the workshop, and we are building this very live.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
