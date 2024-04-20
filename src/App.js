b10bfd9fa32e64edf1a09309da0556b2511ce0f9
Abhishek Tiwari <akt343243@gmail.com>
2024-04-20 17:06:07 +0530

    'changes'

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello from the workshop, and we are building this very live.</p>
+        <p>TechSpectra Workshop</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          follow us
         </a>
       </header>
     </div>
